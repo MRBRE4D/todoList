0947a587f45c102b45812b04d6d8e1ebf55b3e0e
MRBRE4D <justhuangbread@gmail.com>
2024-04-02 19:47:02 +0800

    scrolltoBottom

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,12 +1,11 @@
-import React, { useState} from "react";
-import  { useTodo } from "../hooks/TodoContext";
+import React, { useEffect, useState } from "react";
+import { useTodo } from "../hooks/TodoContext";
 
 import styled from "styled-components";
 import addIcon from "../assets/icon/add.png";
 
 //#region  styled-components
 const Wrapper = styled.div`
-  border: 0.5px solid red;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -53,9 +52,7 @@ const AddBtn = styled.button`
 
 const Control = () => {
   // 解構 Context 傳過來的 "增加todo" 操作
-  const { addTodo } =useTodo()
-  ;
-
+  const { addTodo } = useTodo();
   // todo 內容
   const [todoContent, setTodoContent] = useState("");
 
@@ -64,12 +61,25 @@ const Control = () => {
     // console.log(e.target.value);
   };
 
-  
-  // 點擊"+"按鈕後呼叫addTodo，並disptch ADD_TODO action
-  const handleClick = () => {
+  // 點擊"+"按鈕後將todo內容傳入addTodo，並將輸入框清空
+  const handleClick = (e) => {
+    if (!todoContent) {
+      alert("輸入框不可為空");
+      return
+    }
     addTodo(todoContent);
     setTodoContent("");
   };
+
+  // 放在handleClick裡，滾動會在增加前執行，少一個todo的高度
+  // const scrollToBottom = () => {
+  //   window.scrollTo(0, document.body.scrollHeight);
+  // };
+
+// 監聽增加todo的動作，每次增加就滾到最底
+  useEffect(()=>{
+    window.scrollTo(0, document.body.scrollHeight);
+  },[addTodo])
 
   return (
     <>
@@ -77,11 +87,16 @@ const Control = () => {
         <div>Add to list</div>
         <WrapperInput>
           <Input
+            id="a"
             value={todoContent}
             // onKeyDown={handleClick}
             onChange={handleChange}
           />
-          <AddBtn onClick={() => handleClick()}>
+          <AddBtn
+            onClick={() => {
+              handleClick();
+            }}
+          >
             <img src={addIcon} alt="" />
           </AddBtn>
         </WrapperInput>
